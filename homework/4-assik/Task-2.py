4546f9c1a59b89d5303c0464bdc002feae76b631
Ruinch <163226394+Ruinch@users.noreply.github.com>
2026-01-13 00:12:09 +0500

    task 2 complete

---
+++ homework/4-assik/Task-2.py
@@ -0,0 +1,28 @@
+import json
+
+# read json
+with open("homework/4-assik/students.json", "r", encoding="utf-8") as f:
+    students = json.load(f)
+
+i = 0
+while i < len(students):
+    grades = students[i]["grades"]
+
+    # count sum of grades
+    s = 0
+    j = 0
+    while j < len(grades):
+        s = s + grades[j]
+        j = j + 1
+
+    avg = s / len(grades)
+
+    # add avg
+    students[i]["average"] = avg
+
+    i = i + 1
+
+
+# write new file
+with open("homework/4-assik/students_avg.json", "w", encoding="utf-8") as f:
+    json.dump(students, f, indent=4)
